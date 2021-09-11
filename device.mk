d0344739b087b80d456d0a4637b7bf9d7ccd5dbc
Alexander Koskovich <akoskovich@pm.me>
2021-09-10 20:38:25 -0700

    kenzo: Remove vendor RenderScript implementation.
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: I19460ef266a646b046f1e7d2f0b4eab7c48ae536
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -399,10 +399,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
