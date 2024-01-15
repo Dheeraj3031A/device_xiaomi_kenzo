cb5e8fc2a48f73bbcce0d5ebaac402588c4d4719
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-01-15 20:45:01 +0530

    Update config.fs

--- config.fs
+++ config.fs
@@ -61,12 +61,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [system/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
