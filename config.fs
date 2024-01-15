dc5ce280878ee5e17db4d46e4624ea008b4e9fd1
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-01-15 20:49:20 +0530

    Update config.fs

--- config.fs
+++ config.fs
@@ -73,12 +73,6 @@ user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_RADIO
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_RADIO
