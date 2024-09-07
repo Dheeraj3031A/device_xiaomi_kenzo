a16cbd9b08fd6bf955c9da37f935752c9c42dbe7
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-09-07 22:12:27 +0530

    kenzo: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+# Vendor Tree
+rm -rf vendor/xiaomi/kenzo
+git clone https://github.com/Dheeraj3031A/vendor_xiaomi_kenzo.git vendor/xiaomi/kenzo --depth=1
+
+# Kernel Tree
+rm -rf kernel/xiaomi/kenzo
+git clone https://github.com/Dheeraj3031A/kernel_xiaomi_kenzo.git kernel/xiaomi/kenzo --depth=1
