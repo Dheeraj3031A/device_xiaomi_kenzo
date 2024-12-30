62602611d5b76b0ca96ce01785f26339f9739ec2
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-12-30 20:32:30 +0530

    kenzo: Build Aperture camera

--- device.mk
+++ device.mk
@@ -152,7 +152,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    camera.msm8952 \
     libqomx_core \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
