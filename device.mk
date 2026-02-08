a2e0ec77dc92134b87038a6eede97b9e2d5bd524
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2026-02-08 16:59:36 +0530

    Revert "kenzo: Build Aperture camera"
    This reverts commit 62602611d5b76b0ca96ce01785f26339f9739ec2.

--- device.mk
+++ device.mk
@@ -156,6 +156,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    camera.msm8952 \
     libqomx_core \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
