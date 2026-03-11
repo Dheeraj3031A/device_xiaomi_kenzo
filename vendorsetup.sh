92345a908dab0a4c6e5dcec7e89113135bcb2e2e
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2026-03-11 23:11:36 +0530

    kenzo: vendorsetup: Update kernel branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/Dheeraj3031A/vendor_xiaomi_kenzo.git -b lineage-19.
 
 # Kernel Tree
 rm -rf kernel/xiaomi/kenzo
-git clone https://github.com/Dheeraj3031A/kernel_xiaomi_kenzo.git kernel/xiaomi/kenzo --depth=1
+git clone https://github.com/Dheeraj3031A/kernel_xiaomi_kenzo.git -b Optimus-v3 kernel/xiaomi/kenzo --depth=1
